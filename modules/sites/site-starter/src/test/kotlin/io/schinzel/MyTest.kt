3c92d1d45cf429e4813fd63912cc52abbc2a6631
Schinzel <henrik@schinzel.io>
2023-02-13 07:19:57 +0100

    Code

---
+++ modules/sites/site-starter/src/test/kotlin/io/schinzel/MyTest.kt
@@ -0,0 +1,12 @@
+package io.schinzel
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class MyTest {
+
+    @Test
+    fun theTest(){
+        assertThat(true).isTrue
+    }
+}
