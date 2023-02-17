dd15231b8cf5448e290fb9491cff72b1953bb0f9
Henrik Schinzel <henrik@schinzel.io>
2023-02-17 16:53:42 +0100

    Code

--- modules/sites/admin/src/main/kotlin/project/sites/admin/WebServer.kt
+++ modules/sites/admin/src/main/kotlin/project/sites/admin/WebServer.kt
@@ -1,6 +1,7 @@
 package project.sites.admin
 
 import io.javalin.Javalin
+import io.javalin.http.staticfiles.Location
 
 fun main() {
     WebServer()
@@ -8,7 +9,9 @@ fun main() {
 
 class WebServer {
     init {
-        Javalin.create()
+        Javalin.create { config ->
+            config.staticFiles.add("/site_admin", Location.CLASSPATH)
+        }
             .get("/") { ctx -> ctx.result("Hello Admin World") }
             .start(5555)
     }
--- modules/sites/public/src/main/kotlin/project/sites/public/WebServer.kt
+++ modules/sites/public/src/main/kotlin/project/sites/public/WebServer.kt
@@ -1,14 +1,21 @@
 package project.sites.public
 
 import io.javalin.Javalin
+import io.javalin.http.staticfiles.Location
 
 fun main() {
     WebServer()
 }
 
+/**
+ * http://127.0.0.1:5555/
+ * http://127.0.0.1:5555/index.html
+ */
 class WebServer {
     init {
-        val javalin = Javalin.create()
+        Javalin.create { config ->
+            config.staticFiles.add("/site_public", Location.CLASSPATH)
+        }
             .get("/") { ctx -> ctx.result("Hello Public World") }
             .start(5555)
     }
